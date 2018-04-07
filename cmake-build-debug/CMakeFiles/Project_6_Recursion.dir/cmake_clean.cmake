21cf49837f8d91e92c6d1e31ecb0e979af38aa72
Armin Taher <armin_taher@utexas.edu>
2018-04-07 11:58:44 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Project_6_Recursion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_6_Recursion.dir/main.cpp.o"
+  "CMakeFiles/Project_6_Recursion.dir/Project6.cpp.o"
+  "Project_6_Recursion.pdb"
+  "Project_6_Recursion.exe"
+  "libProject_6_Recursion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_6_Recursion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
